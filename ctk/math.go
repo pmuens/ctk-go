30a8a7e8ea125bad2b9e94a5b30fd06be3234a5b
Philipp Muens <philipp@muens.io>
2024-08-16 15:01:32 +0200

    Add error example

--- ctk/math.go
+++ ctk/math.go
@@ -1,5 +1,10 @@
 package ctk
 
+const (
+	// ErrDivisionByZero is returned if there's an attempt to divide by zero.
+	ErrDivisionByZero = Error("division by zero")
+)
+
 // Mul multiplies a by b and returns the result.
 func Mul(a, b int) int {
 	result := 0
@@ -9,6 +14,16 @@ func Mul(a, b int) int {
 	}
 
 	return result
+}
+
+// Div divides a by b and returns the rounded-down result.
+// Returns an error if b is zero (division by zero).
+func Div(a, b int) (int, error) {
+	if b == 0 {
+		return 0, ErrDivisionByZero
+	}
+
+	return a / b, nil
 }
 
 // add adds a and b and returns the result.
--- ctk/math_test.go
+++ ctk/math_test.go
@@ -1,6 +1,7 @@
 package ctk_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/pmuens/ctk-go/ctk"
@@ -22,6 +23,37 @@ func TestMul(t *testing.T) {
 			t.Parallel()
 
 			got := ctk.Mul(tc.a, tc.b)
+
+			if got != tc.expected {
+				t.Errorf("expected %v, got %v", tc.expected, got)
+			}
+		})
+	}
+}
+
+func TestDiv(t *testing.T) {
+	tt := map[string]struct {
+		a        int
+		b        int
+		expected int
+		err      error
+	}{
+		"2 / 1":  {a: 2, b: 1, expected: 2, err: nil},
+		"1 / 0":  {a: 1, b: 0, expected: 0, err: ctk.ErrDivisionByZero},
+		"2 / 2":  {a: 2, b: 2, expected: 1, err: nil},
+		"12 / 2": {a: 12, b: 2, expected: 6, err: nil},
+		"3 / 2":  {a: 3, b: 2, expected: 1, err: nil},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := ctk.Div(tc.a, tc.b)
+
+			if !errors.Is(err, tc.err) {
+				t.Errorf("expected error %v, got %v", tc.err, err)
+			}
 
 			if got != tc.expected {
 				t.Errorf("expected %v, got %v", tc.expected, got)
