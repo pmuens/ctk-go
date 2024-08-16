d455c3a1da5269bb4ea99a73757b651120155749
Philipp Muens <philipp@muens.io>
2024-08-16 15:09:00 +0200

    Add doc.go

---
+++ ctk/doc.go
@@ -0,0 +1,7 @@
+/*
+Package ctk exposes an API for mathematical operations.
+
+Use the ctk.Mul function to perform a multiplication of two numbers.
+The ctk.Div function can be used to divide a number a by a second number b.
+*/
+package ctk
