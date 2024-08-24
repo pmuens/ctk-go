d1d375a3f2c4c9fff183e519371d9e08b40063f8
Philipp Muens <philipp@muens.io>
2024-08-24 20:56:58 +0200

    Implement quarter round function for ChaCha state

--- ctk/chacha20.go
+++ ctk/chacha20.go
@@ -4,6 +4,26 @@ import (
 	"math/bits"
 )
 
+// ChaCha is a stateful instance of the ChaCha stream cipher.
+type ChaCha struct {
+	// state is the internal state on which operations are performed.
+	state [16]uint32
+}
+
+// quarterRound is an implementation of the ChaCha quarter round function	that
+// permutes ChaCha's internal state.
+// x, y, z, and w are used to index into the state.
+func (s *ChaCha) quarterRound(x, y, z, w int) [16]uint32 {
+	a, b, c, d := quarterRound(s.state[x], s.state[y], s.state[z], s.state[w])
+
+	s.state[x] = a
+	s.state[y] = b
+	s.state[z] = c
+	s.state[w] = d
+
+	return s.state
+}
+
 // quarterRound is an implementation of the ChaCha quarter round function.
 func quarterRound(a, b, c, d uint32) (uint32, uint32, uint32, uint32) {
 	a += b
--- ctk/chacha20_internal_test.go
+++ ctk/chacha20_internal_test.go
@@ -18,4 +18,29 @@ func TestChaCha20QuarterRound(t *testing.T) {
 			t.Errorf("want %v, got %v", want, got)
 		}
 	})
+
+	t.Run("RFC 8439 - Test Vectors - 2.2.1", func(t *testing.T) {
+		t.Parallel()
+
+		cha := ChaCha{
+			state: [16]uint32{
+				0x879531e0, 0xc5ecf37d, 0x516461b1, 0xc9a62f8a,
+				0x44c20ef3, 0x3390af7f, 0xd9fc690b, 0x2a5f714c,
+				0x53372767, 0xb00a5631, 0x974c541a, 0x359e9963,
+				0x5c971061, 0x3d631689, 0x2098d9d6, 0x91dbd320,
+			},
+		}
+
+		got := cha.quarterRound(2, 7, 8, 13)
+		want := [16]uint32{
+			0x879531e0, 0xc5ecf37d, 0xbdb886dc, 0xc9a62f8a,
+			0x44c20ef3, 0x3390af7f, 0xd9fc690b, 0xcfacafd2,
+			0xe46bea80, 0xb00a5631, 0x974c541a, 0x359e9963,
+			0x5c971061, 0xccc07c79, 0x2098d9d6, 0x91dbd320,
+		}
+
+		if got != want {
+			t.Errorf("want %v, got %v", want, got)
+		}
+	})
 }
